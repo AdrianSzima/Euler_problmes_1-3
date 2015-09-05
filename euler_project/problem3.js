89ebfc098d70f44d09eb47d595d62c71563c3086
AdrianSzima <adrsz99@gmail.com>
2015-09-05 15:46:42 -0700

    Euler project problems 1-3

---
+++ euler_project/problem3.js
@@ -0,0 +1,30 @@
+
+
+/*The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 or 1000(as specified by Odin) ? */
+
+
+
+
+
+
+
+
+
+
+var primeFactor = function (number) {
+    var i = 2;
+	while(i<=number) {
+        if (number % i === 0 && number/i !== 1) {
+            number = number/i;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return number;
+};
+
+console.log(primeFactor(1000));
